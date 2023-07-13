bbdc97e8999db88e10e78e9df008403b9aa147ed
mehdirahman88 <hide.m88@gmail.com>
2023-07-14 05:10:13 +0700

    Refactor

--- diet/permissions.py
+++ diet/permissions.py
@@ -9,9 +9,13 @@ class IsManagerFitnessUser(BasePermission):
     """
 
     def has_permission(self, request, view):
-        return bool(request.user and hasattr(request.user, 'role') and request.user.role == UserRoleChoice.MANAGER)
+        return bool(request.user and is_manager_fitness_user(request.user))
 
 
 class IsDietRecordOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.fitness_user == request.user
+
+
+def is_manager_fitness_user(user):
+    return hasattr(user, 'role') and user.role == UserRoleChoice.MANAGER
--- diet/views.py
+++ diet/views.py
@@ -12,16 +12,17 @@ class DietRecordListCreate(generics.ListCreateAPIView):
     serializer_class = serializers.DietRecordListCreateSerializer
 
     def get_queryset(self):
-        if self.request.user.role == UserRoleChoice.MANAGER or self.request.user.is_staff:
+        if custom_permissions.is_manager_fitness_user(self.request.user) or self.request.user.is_staff:
             queryset = DietRecord.objects.all()
             return queryset
         raise PermissionDenied("You are not allowed to view all records.")
 
     def perform_create(self, serializer):
         fitness_user_id_in_request = serializer.validated_data['fitness_user_id']
-        if fitness_user_id_in_request != self.request.user.id:
-            raise PermissionDenied("You are not allowed to create a DietRecord for another user.")
-        serializer.save()
+        if fitness_user_id_in_request == self.request.user.id:
+            serializer.save()
+            return
+        raise PermissionDenied("You are not allowed to create a DietRecord for another user.")
 
 
 class DietRecordRWD(generics.RetrieveUpdateDestroyAPIView):
@@ -35,8 +36,9 @@ class UserDietRecordList(generics.ListAPIView):
     serializer_class = serializers.DietRecordRWDSerializer
 
     def get_queryset(self):
-        user_id = self.kwargs['user_id']
-        if user_id != self.request.user.id and self.request.user.role != UserRoleChoice.MANAGER:
-            raise PermissionDenied("You are not allowed to view.")
-        queryset = DietRecord.objects.filter(fitness_user_id=user_id)
-        return queryset
+        user_id_in_url = self.kwargs['user_id']
+        logged_in_user = self.request.user
+        if user_id_in_url == logged_in_user.id or custom_permissions.is_manager_fitness_user(logged_in_user):
+            queryset = DietRecord.objects.filter(fitness_user_id=user_id_in_url)
+            return queryset
+        raise PermissionDenied("You are not allowed to view.")
